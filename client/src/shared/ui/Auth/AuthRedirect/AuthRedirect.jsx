ec0d3881c3a625f7c9a11a269968c39697f1c29c
Hov <sadavid565@gmail.com>
2026-04-17 18:05:08 +0300

    Page navigaion fix

--- client/src/shared/ui/Auth/AuthRedirect/AuthRedirect.jsx
+++ client/src/shared/ui/Auth/AuthRedirect/AuthRedirect.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom"
+
 const AuthRedirect = ({ type }) => {
     return type === 'Login'
         ? (
             <div className="form-redirect">
-                Нет аккаунта? <a href="/register" className="link" id="registerRedirect">Создать кибер-аккаунт →</a>
+                Нет аккаунта? <Link to="/register" className="link" id="registerRedirect">Создать кибер-аккаунт →</Link>
             </div>
         )
         : (
             <div className="form-redirect">
-                Уже есть аккаунт? <a href="/login" className="link" id="loginRedirect">Войти в систему →</a>
+                Уже есть аккаунт? <Link to='/login'className="link" id="loginRedirect">Войти в систему →</Link>
             </div>
         )
 }
--- client/src/shared/ui/Header/HeaderBrand/HeaderBrand.jsx
+++ client/src/shared/ui/Header/HeaderBrand/HeaderBrand.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './HeaderBrand.css'
 
 const HeaderBrand = (props) => {
@@ -6,16 +7,18 @@ const HeaderBrand = (props) => {
     } = props
 
     return (
-        <div className="brand">
-            <div className="tetris-icon">
-                <i className="fas fa-cubes"></i>
-            </div>
+        <Link to='/' >
+            <div className="brand">
+                <div className="tetris-icon">
+                    <i className="fas fa-cubes"></i>
+                </div>
 
-            <div className="logo-text">
-                <h1 className='glow-text'>⚡ PVP TETRIS ⚡</h1>
-                {description ? <p className='description'>BATTLE ARENA • NEO TOURNAMENT</p> : ''}
+                <div className="logo-text">
+                    <h1 className='glow-text'>⚡ PVP TETRIS ⚡</h1>
+                    {description ? <p className='description'>BATTLE ARENA • NEO TOURNAMENT</p> : ''}
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
